d4cf07f49104e9608ce0fc9a002f63d03946428a
Tyler Bird <birdty@gmail.com>
2016-05-12 15:57:17 -0400

    o - no comment

---
+++ Bibliopolis/Entity/Customer.pm
@@ -0,0 +1,27 @@
+package Bibiliopolis::Entity::Customer;
+
+sub _db_name { return 'bibliopolis'; }
+
+sub _db_table_name { return 'customer'; }
+
+sub _db_table_id_column { return 'id'; }
+
+sub _db_table_columns
+{
+  return qw(
+    id                  
+    first_name          
+    last_name           
+    email_address       
+    billing_address1    
+    billing_address2    
+    billing_city        
+    billing_state       
+    billing_postal_code 
+    billing_phone       
+  );
+}
+
+sub has_field_disabled { return 0; }
+
+1;
