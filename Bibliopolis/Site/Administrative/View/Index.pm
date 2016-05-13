e058c7ff02a79543fd9de98bc178391ac452ef06
Tyler Bird <birdty@gmail.com>
2016-05-13 12:23:36 -0400

    o - refactored to clean up shell specific stuff

--- Bibliopolis/Site/Administrative/View/Index.pm
+++ Bibliopolis/Site/Administrative/View/Index.pm
@@ -6,20 +6,15 @@ sub default
 {
     my $self = shift;
 
-    use MIME::Types;
-
-    my $mime_types = MIME::Types->new();
-    my $html_type = $mime_types->type('text/html');
-
-    print("Content-type: " . $html_type . "\n\n");
-
     my $shell = $self->find_shell('type' => 'html');
 
     my $contents = $self->read_template('index.tpl');
 
-    $shell->merge({'contents' => $contents});
-
-    print $shell;
+    if ( $contents )
+    {	
+      $shell->merge({'contents' => $contents});
+      print $shell;
+    }
 }
 
 1;
