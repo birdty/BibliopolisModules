e6d7b6f443ef4d537c40317184c10e41d60ad43b
Tyler Bird <birdty@gmail.com>
2016-05-13 09:19:43 -0400

    o - initial add

---
+++ Bibliopolis/Entity/Item.pm
@@ -0,0 +1,20 @@
+package Bibiliopolis::Entity::Item;
+
+sub _db_name { return 'bibliopolis'; }
+
+sub _db_table_name { return 'item'; }
+
+sub _db_table_id_column { return 'id'; }
+
+sub _db_table_columns
+{
+  return qw(
+    id                  
+    account_id
+    name
+  );
+}
+
+sub has_field_disabled { return 0; }
+
+1;
